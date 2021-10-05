7b5a7dc10803b0c801aa63537541abe36671c6e2
Perla D. Canario Perez <perladenis163@gmail.com>
2021-10-05 01:25:04 -0400

    creacion de la BLL y el registro

---
+++ BLL/AportesBLL.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_PerlaD._20190008.DAL;
+using P1_AP1_PerlaD._20190008.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_PerlaD._20190008.BLL
+{
+    public class AportesBLL
+    {
+        public static bool Guardar(Aportes aportes)
+        {
+            if (!Existe(aportes.AportesId))
+                return Insertar(aportes);
+            else
+                return Editar(aportes);
+        }
+        private static bool Insertar(Aportes aportes)
+        {
+            bool paso = false;
+            Aportes aporte = new Aportes();
+            try
+            {
+                aportes.Aporte.Add(aportes);
+                paso = aporte.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Editar(Aportes aportes)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(aportes).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var ciudad = contexto.Aportes.Find(id);
+                if (ciudad != null)
+                {
+                    contexto.Aportes.Remove(ciudad);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Aportes Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Aportes aportes;
+            try
+            {
+                aportes = contexto.Aportes.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return aportes;
+        }
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Aportes
+                    .Any(e => e.AportesId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+        public static bool Existe(string Persona)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Aportes
+                    .Any(e => e.Persona == Persona);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+        public static List<Aportes> GetList(Expression<Func<Aportes, bool>> criterio)
+        {
+            List<Aportes> lista = new List<Aportes>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Aportes.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        public static List<Aportes> GetList()
+        {
+            List<Aportes> lista = new List<Aportes>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Aportes.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ UI/Registro/rAportes.xaml.cs
@@ -0,0 +1,106 @@
+﻿using P1_AP1_PerlaD._20190008.BLL;
+using P1_AP1_PerlaD._20190008.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_PerlaD._20190008.UI.Registro
+{
+    /// <summary>
+    /// Lógica de interacción para rAportes.xaml
+    /// </summary>
+    public partial class rAportes : Window
+    {
+        private Aportes aportes;
+        public rAportes()
+        {
+            InitializeComponent();
+            aportes = new Aportes();
+            this.DataContext = aportes;
+        }
+        private void Limpiar()
+        {
+            this.aportes = new Aportes();
+            this.DataContext = aportes;
+        }
+        public void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+        private bool Validar()
+        {
+            bool esValido = true;
+            if (AportesBLL.Existe(PersonaTextBox.Text))
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, no puede repetir el nombre", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+;           if (PersonaTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, debe insertar un nombre", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            if (PersonaTextBox.Text.Any(char.IsDigit))
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, no hay ciudades con numeros", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            return esValido;
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ciudad = AportesBLL.Buscar(Convert.ToInt32(AportesIdTextBox.Text));
+            if (ciudad != null)
+                this.aportes = ciudad;
+            else
+            {
+                this.aportes = new Aportes();
+                MessageBox.Show("No se ha encontrado", "Error",
+                   MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            this.DataContext = this.aportes;
+        }
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!Validar())
+                return;
+            var paso = AportesBLL.Guardar(this.aportes);
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Transaccion exitosa", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AportesBLL.Eliminar(Convert.ToInt32(AportesIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+    }
+}
