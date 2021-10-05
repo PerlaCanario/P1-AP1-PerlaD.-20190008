310dd1263a852a0b9c2ad183635bf2864aeb40ed
Perla D. Canario Perez <perladenis163@gmail.com>
2021-10-04 22:16:40 -0400

    Contexto creado

---
+++ DAL/Cotexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_PerlaD._20190008.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_PerlaD._20190008.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\DBP1-PerlaD.-20190008.db");
+        }
+    }
+}
